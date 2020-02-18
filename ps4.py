dc7e32a7083fb19e4b517a720eb3e034bf8cc64b
tlincke125 <thli2739@colorado.edu>
2020-02-18 07:56:26 -0700

    added adaptive time step

--- ps4.py
+++ ps4.py
@@ -2,13 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-        
-
-
+# Simple linear equation        
 class LinearEqu:
 
-    def __init__(self, state_func, size, start_state = None):
+    def __init__(self, state_func, size, tolerance = 0, start_state = None):
 
         self.size = size
 
@@ -29,35 +26,54 @@ class LinearEqu:
         # Time
         self.time = 0.0
 
-    def de_step_rk4(self, h, t):
-        k1 = h * self.func(state = self.state, t = t)
-        k2 = h * self.func(state = self.state + k1 / 2, t = t + h / 2)
-        k3 = h * self.func(state = self.state + k2 / 2, t = t + h / 2)
-        k4 = h * self.func(state = self.state + k3, t = t + h)
+    def de_step_rk4(self, h, t, x):
+        k1 = h * self.func(state = x, t = t)
+        k2 = h * self.func(state = x + k1 / 2, t = t + h / 2)
+        k3 = h * self.func(state = x + k2 / 2, t = t + h / 2)
+        k4 = h * self.func(state = x + k3, t = t + h)
 
-        self.next_state = self.state + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        self.time = self.time + h
+        # Return new h, new state
+        return t + h, (x + (k1 + 2 * k2 + 2 * k3 + k4) / 6)
 
-    def de_euler(self, h, t):
-        self.next_state = self.state + h * self.func(self.state, t)
+
+    def step(self, h, adaptive_t):
+        if adaptive_t:
+
+            # h step
+            t1, x1 = self.de_step_rk4(h, self.time, self.state)
+
+            # h / 2 step
+            t2, x2 = self.de_step_rk4(h / 2, self.time, self.state)
+            t3, x3 = self.de_step_rk4(h / 2, t1, x2)
+
+            error = np.linalg.norm(x3 - x1)
+
+    
+        
+
+
+
+            t1, x1 = self.de_step_rk4(h, self.time)
+            t1, 
+            t1, x1 = self.de_step_rk4(h / 2, self.time)
+
+            self.time, self.next_state = self.de_step_rk4(h, self.time)
             
 
-    def step(self, h, func="rk4"):
-        if(func == "rk4"):
-            self.de_step_rk4(h, self.time)
-            self.next_state, self.state = self.state, self.next_state
-        elif(func == "euler"):
-            self.de_euler(h, self.time)
-            self.next_state, self.state = self.state, self.next_state
-        else:
-            print("unknown method")
 
-    def ode(self, h, n, func_type="rk4"):
-        t = np.zeros(n, dtype=float) 
+        if not adaptive_t:
+            # Update t, x_next
+            self.time, self.next_state = self.de_step_rk4(h, self.time)
+            # Swap next with current
+            self.next_state, self.state = self.state, self.next_state
+
+
+    def ode(self, h, n, adaptive_t = False):
+        # An array of state vectors
         states = np.zeros(n * self.size).reshape(self.size, n)
 
         for i in range(n):
-            self.step(h, func_type)
+            self.step(h, adaptive_t)
             states[:,i] = self.state
 
         return states
@@ -79,7 +95,25 @@ def theta(state, t):
 def omega(state, t, beta, l, alpha, A, m, g):
     return (A * np.cos(alpha * t) - beta * l * state[1] - m * g * np.sin(state[0])) / (m * l)
 
+def omega2(state, t, beta, l, alpha, A, m, g):
+    return (A * np.cos(alpha * t) - beta * l * state[1] - m * g * state[0]) / (m * l)
+
+
 def func(state, t):
+    beta = 0
+    l = 0.1
+    m = 0.1
+    g = 9.8
+
+    natfreq = np.sqrt(g / l)
+
+    A = 0
+    alpha = 0
+
+    return np.array([theta(state, t), \
+            omega(state, t, beta = beta, l = l, alpha = alpha, A = A, m = m, g = g)])
+
+def func2(state, t):
     beta = 0
     l = 0.1
     m = 0.1
@@ -118,34 +152,45 @@ if __name__ == '__main__':
               #  [7, -50.0]]
 
 
-    a = LinearEqu(func, 2, [3.0, 0.1])
-    output = a.ode(1, 300, "rk4")
-
-    output[0] = output[0] 
-    plt.plot(output[0], output[1], label="(Om_0, w_0) = (3, 0.1)")
-    plt.xlabel("Theta (rad)")
-    plt.ylabel("Omega (rad / s)")
-    plt.title("State Space Trajectory Nonlinear Pendulum")
-    plt.plot(output[0][0],output[1][0],'ro', label="Starting Point")
-    plt.legend()
-
-    #
-    #  for i in states:
-    #      a.reset(i)
-    #      output = a.ode(0.005, 1000, "rk4")
-    #      th = output[0]
-    #      om = output[1]
-    #
-    #      th = th % (2 * np.pi)
-    #
-    #      plt.scatter(th, om, s=2)
+    a = LinearEqu(func, 2, [0.0, 1.0])
+    b = LinearEqu(func2, 2, [0.0, 1.0])
+    #  output1 = a.ode(0.005, 3000, "rk4")
+    #  output2 = b.ode(0.005, 3000, "rk4")
 
 
-    
-    #  plt.title("State Space Portrait Nonlinear Damped Pendulum Mod 2pi")
+    #  output[0] = output[0]
+    #  plt.plot(output[0], output[1], label="(Om_0, w_0) = (3, 0.1)")
     #  plt.xlabel("Theta (rad)")
-    #  plt.ylabel("Omega (rad / sec)")
-    #  plt.xlim([0, 2 * np.pi])
+    #  plt.ylabel("Omega (rad / s)")
+    #  plt.title("State Space Trajectory Nonlinear Pendulum")
+    #  plt.plot(output[0][0],output[1][0],'ro', label="Starting Point")
+    #  plt.legend()
+
+
+    for i in states:
+        b.reset(i)
+        a.reset(i)
+        output1 = a.ode(0.005, 1000, "rk4")
+        output2 = b.ode(0.005, 1000, "rk4")
+
+        th1 = output1[0]
+        om1 = output1[1]
+
+        th2 = output2[0]
+        om2 = output2[1]
+
+
+        #  th = th % (2 * np.pi)
+
+        plt.plot(th1, om1)
+        plt.plot(th2, om2)
+
+
+
+    plt.title("State Space Portrait Nonlinear Damped Pendulum Mod 2pi")
+    plt.xlabel("Theta (rad)")
+    plt.ylabel("Omega (rad / sec)")
+    plt.xlim([-2 * np.pi, 2 * np.pi])
     plt.show()
 
 
---
+++ ps5.py
@@ -0,0 +1,166 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
+class LinearEqu:
+
+    def __init__(self, state_func, size, tolerance = 100000, start_state = None, time_step = 0.001):
+        self.size = size
+        self.func = state_func
+        self.h = time_step
+        self.time = 0.0
+        self.tolerance = tolerance
+
+        if start_state == None or len(start_state) != self.size:
+            self.state = np.zeros(self.size)
+        else:
+            self.state = np.array(start_state)
+
+        
+    def de_step_rk4(self, h, t, x):
+        k1 = h * self.func(state = x, t = t)
+        k2 = h * self.func(state = x + k1 / 2, t = t + h / 2)
+        k3 = h * self.func(state = x + k2 / 2, t = t + h / 2)
+        k4 = h * self.func(state = x + k3, t = t + h)
+
+        return t + h, (x + (k1 + 2 * k2 + 2 * k3 + k4) / 6)
+
+    def step(self, adaptive_t):
+        if adaptive_t:
+
+            error = self.tolerance + 1
+            i = 10
+            
+            # One step forward
+            t1, x1 = self.de_step_rk4(self.h, self.time, self.state)
+
+            # Two steps forward
+            t2, x2 = self.de_step_rk4(self.h / 2, self.time, self.state)
+            t3, x3 = self.de_step_rk4(self.h / 2, t2, x2)
+
+            error = np.linalg.norm(x3 - x1)
+
+            # This could be done with recursion,
+            # But recursion is evil
+    
+
+            if(error > self.tolerance):
+                while error > self.tolerance and i > 0:
+                    self.h = self.h / 2
+
+                    # One step forward
+                    t1, x1 = self.de_step_rk4(self.h, self.time, self.state)
+
+                    # Two steps forward
+                    t2, x2 = self.de_step_rk4(self.h / 2, self.time, self.state)
+                    t3, x3 = self.de_step_rk4(self.h / 2, t2, x2)
+
+                    error = np.linalg.norm(x3 - x1)
+                    i -= 1
+
+                if i <= 0:
+                    print("Greater Than Exceeded depth")
+
+                # Pick the best step no matter what
+                self.time, self.state = t3, x3
+
+            elif(error < self.tolerance):
+
+                # Pick the best step
+                self.time, self.state = t3, x3
+
+                while error < self.tolerance and i > 0:
+                    self.h = self.h * 2
+
+                    # One step forward
+                    t1, x1 = self.de_step_rk4(self.h, self.time, self.state)
+
+                    # Two steps forward
+                    t2, x2 = self.de_step_rk4(self.h / 2, self.time, self.state)
+                    t3, x3 = self.de_step_rk4(self.h / 2, t2, x2)
+
+                    error = np.linalg.norm(x3 - x1)
+                    i -= 1
+
+                if i <= 0:
+                    print("Less Than Exceeded depth")
+
+            return
+        else:
+            t1, x1 = self.de_step_rk4(self.h, self.time, self.state)
+            self.time, self.state = t1, x1
+            return
+
+    def ode(self, n, adaptive_t = False):
+        time = np.zeros(n)
+        states = np.zeros(n * self.size).reshape(self.size, n)
+
+        for i in range(n):
+            self.step(adaptive_t)
+            time[i] = self.time
+            states[:,i] = self.state
+
+        return time, states
+
+    def reset(self, start_state = None):
+        if start_state == None or len(start_state) != self.size:
+            self.state = np.zeros(self.size)
+        else:
+            self.state = np.array(start_state)
+
+        self.time = 0.0
+
+#  a = 16
+#  r = 25
+#  b = 4
+
+
+a = 0.398
+b = 2
+c = 4
+
+#  def func(state, t):
+#      xdot = a * (state[1] - state[0])
+#      ydot = r * state[0] - state[0] - state[0] * state[2]
+#      zdot = state[0] * state[1] - b * state[2]
+#      return np.array([xdot, ydot, zdot])
+#
+def func_rossler(state, t):
+    xdot = -(state[1] + state[2])
+    ydot = state[0] + a * state[1]
+    zdot = b + state[2] * (state[0] - c)
+    return np.array([xdot, ydot, zdot])
+
+
+if __name__ == '__main__':
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    start = [-13, -12, 52]
+
+    lorentz = LinearEqu(state_func = func_rossler, size = 3, tolerance = 30, start_state = start, time_step = 1)
+    t, states = lorentz.ode(10000, True)
+    #  lorentz.reset([-13, -12, 52])
+    #  lorentz.h = 0.001
+    #  t, states_2 = lorentz.ode(50, False)
+
+
+    x = states[0]
+    y = states[1]
+    z = states[2]
+
+    #  x_2 = states_2[0]
+    #  y_2 = states_2[1]
+    #  z_2 = states_2[2]
+    #
+    #  ax.scatter(x_2, y_2, z_2, zdir='z', c='blue', s=10, label="Stationary T Step")
+    ax.scatter(x, y, z, zdir='z', c='black', label="Adaptive T Step", s=1)
+
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title(f'Rossler Adaptive Time Step')
+    plt.legend()
+    plt.show()
